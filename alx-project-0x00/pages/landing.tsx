031c916ec122c321cb5f5b250b976bb8850f5cf4
baby6969 <yafetzekarias23@fmail.com>
2025-11-23 12:43:15 +0300

    update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,13 @@
-const Landing: React.FC =  () => {
+import React from 'react';
+import Card from "@/components/Card"
+
+const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
     </div>
+    
   )
 }
 export default Landing
